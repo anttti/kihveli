41dc6106a863a2b4df5fdf16ca996176e4615933
Antti Mattila <me@rarelyneeded.com>
2020-05-16 19:14:15 +0300

    Add Netlify Identity

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import Head from "next/head";
 import { Provider } from "overmind-react";
 import { useOvermindFromPageProps } from "../overmind";
 import "../styles/index.css";
@@ -5,11 +7,26 @@ import "../styles/index.css";
 export default function App({ Component, pageProps }) {
   const overmind = useOvermindFromPageProps(pageProps);
 
+  useEffect(() => {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", (user) => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+  }, []);
+
   // Need to null-check Overmind, otherwise useOvermind-hook will crash when
   // Overmind is null on the initial render
   if (overmind) {
     return (
       <Provider value={overmind}>
+        <Head>
+          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        </Head>
         <Component {...pageProps} />
       </Provider>
     );
